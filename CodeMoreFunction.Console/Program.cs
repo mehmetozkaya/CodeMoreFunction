8bd43cae4eebb8e98baa169152fafaa603990201
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-24 20:14:35 +0300

    Initial

---
+++ CodeMoreFunction.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeMoreFunction.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CodeMoreFunction.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CodeMoreFunction.Core
+{
+    public class Class1
+    {
+    }
+}
