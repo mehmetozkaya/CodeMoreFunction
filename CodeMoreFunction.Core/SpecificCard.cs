f124c45f4e89342fb70594cb9cf30c6b1c2b85de
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-25 09:18:28 +0300

    SpecificCard created

---
+++ CodeMoreFunction.Core/SpecificCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeMoreFunction.Core
+{
+    public class SpecificCard : SpecificMoney
+    {
+        private BankCard Card { get; }
+
+        public SpecificCard(Currency currency, BankCard card) : base(currency)
+        {
+            this.Card = card ?? throw new ArgumentNullException(nameof(card));
+        }
+
+        public override Money On(Timestamp time) =>
+            new SpecificCard(this.Currency, this.Card.CardOn(time));
+
+        public override Tuple<Amount, Money> Take(decimal amount) =>
+            this.Card.Take(base.Currency, amount);
+    }
+}
