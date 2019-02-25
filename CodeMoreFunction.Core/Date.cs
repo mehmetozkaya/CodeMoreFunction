240ad4cb40f86b6cd761d42bebc43e9d7507bc9e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-25 08:26:10 +0300

    CompareTo

--- CodeMoreFunction.Core/Date.cs
+++ CodeMoreFunction.Core/Date.cs
@@ -2,7 +2,23 @@
 
 namespace CodeMoreFunction.Core
 {
-    public sealed class Date : IEquatable<Date>
+    public sealed class Date : IEquatable<Date>, IComparable<DateTime>, IComparable<Date>, IComparable<Month>, IComparable<Timestamp>
     {
+        private DateTime Value { get; }
+
+        public Date(int year, int month, int day)
+        {
+            this.Value = new DateTime(year, month, day);
+        }
+
+        public bool Equals(Date other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Value.Equals(other.Value);
+        }
+
+        public int CompareTo(DateTime other) =>
+            this.Value.CompareTo(other);
     }
 }
