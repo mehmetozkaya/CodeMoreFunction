cda705f3ea6d02e3e3666c7bdbe75ffb1570a7b4
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-25 08:43:06 +0300

    CardExpired created

---
+++ CodeMoreFunction.Core/BankCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeMoreFunction.Core
+{
+    public class BankCard : Money
+    {
+        public Month ValidBefore { get; }
+
+        public BankCard(Month validBefore)
+        {
+            this.ValidBefore = validBefore ?? throw new ArgumentNullException(nameof(validBefore));
+        }
+
+        public override Money On(Timestamp time) =>
+            this.CardOn(time);
+
+        public BankCard CardOn(Timestamp time) =>
+            time.CompareTo(this.ValidBefore) >= 0
+                ? (BankCard)new CardExpired(this.ValidBefore)
+                : this;
+
+        public override SpecificMoney Of(Currency currency) =>
+            new SpecificCard(currency, this);
+
+        public virtual Tuple<Amount, Money> Take(Currency currency, decimal amount) =>
+            Tuple.Create(new Amount(currency, amount), (Money)this);
+    }
+}
---
+++ CodeMoreFunction.Core/CardExpired.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeMoreFunction.Core
+{
+    public class CardExpired : BankCard
+    {
+        public CardExpired(Month validBefore) : base(validBefore)
+        {
+        }
+
+        public override Money On(Timestamp time) =>
+            this;
+
+        public override SpecificMoney Of(Currency currency) =>
+            new Empty(currency);
+
+        public override Tuple<Amount, Money> Take(Currency currency, decimal amount) =>
+            Tuple.Create(Amount.Zero(currency), (Money)this);
+    }
+}
