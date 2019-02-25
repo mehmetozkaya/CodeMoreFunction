fd9b998dc24c3490348318f7d86ea7d069d37872
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-25 08:31:05 +0300

    Month created

--- CodeMoreFunction.Core/Month.cs
+++ CodeMoreFunction.Core/Month.cs
@@ -1,6 +1,52 @@
-﻿namespace CodeMoreFunction.Core
+﻿using System;
+
+namespace CodeMoreFunction.Core
 {
-    public class Month
+    public sealed class Month : IEquatable<Month>, IComparable<DateTime>, IComparable<Date>, IComparable<Month>, IComparable<Timestamp>
     {
+        private Date Value { get; }
+
+        public Month(int year, int month)
+        {
+            this.Value = new Date(year, month, 1);
+        }
+
+        public bool Equals(Month other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(Value, other.Value);
+        }
+
+        public int CompareTo(DateTime other) =>
+            this.Value.CompareTo(other);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj is Month && Equals((Month)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Value != null ? Value.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Month a, Month b) =>
+            (object.ReferenceEquals(a, null) && object.ReferenceEquals(b, null)) ||
+            (!object.ReferenceEquals(a, null) && a.Equals(b));
+
+        public static bool operator !=(Month a, Month b) => !(a == b);
+
+        public int CompareTo(Timestamp other) =>
+            -other.CompareTo(this.Value);
+
+        public int CompareTo(Date other) =>
+            -other.CompareTo(this.Value);
+
+        public int CompareTo(Month other) =>
+            this.Value.CompareTo(other.Value);
+
     }
 }
