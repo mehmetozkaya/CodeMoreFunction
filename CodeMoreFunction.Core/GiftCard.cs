c0c937fb331c92aacf22f7ecf9af22df271cdcba
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-25 08:43:41 +0300

    GiftCard

---
+++ CodeMoreFunction.Core/GiftCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeMoreFunction.Core
+{
+    public class GiftCard : Amount
+    {
+        public Date ValidBefore { get; }
+
+        public GiftCard(Currency currency, decimal amount, Date validBefore) : base(currency, amount)
+        {
+            this.ValidBefore = validBefore ?? throw new ArgumentNullException(nameof(ValidBefore));
+        }
+
+        public override Money On(Timestamp time) =>
+            time.CompareTo(this.ValidBefore) >= 0
+                ? Amount.Zero(base.Currency)
+                : this;
+    }
+}
