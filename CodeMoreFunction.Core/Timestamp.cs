5ad947d1a6df20cf6d15973544d15dd37f51e4ce
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-25 08:32:38 +0300

    Timestamp created

--- CodeMoreFunction.Core/Timestamp.cs
+++ CodeMoreFunction.Core/Timestamp.cs
@@ -4,5 +4,28 @@ namespace CodeMoreFunction.Core
 {
     public class Timestamp : IComparable<DateTime>, IComparable<Timestamp>, IComparable<Date>, IComparable<Month>
     {
+        private DateTime UtcTime { get; }
+
+        public Timestamp(DateTime utcTime)
+        {
+            if (utcTime.Kind != DateTimeKind.Utc)
+                throw new ArgumentException("UTC time expected.");
+            UtcTime = utcTime;
+        }
+
+        public static Timestamp Now =>
+            new Timestamp(DateTime.UtcNow);
+
+        public int CompareTo(DateTime other) =>
+            this.UtcTime.CompareTo(other);
+
+        public int CompareTo(Timestamp other) =>
+            this.UtcTime.CompareTo(other.UtcTime);
+
+        public int CompareTo(Date other) =>
+            -other.CompareTo(this.UtcTime);
+
+        public int CompareTo(Month other) =>
+            -other.CompareTo(this.UtcTime);
     }
 }
