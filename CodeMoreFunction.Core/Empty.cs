d7875a9af4c310862ad312119337099e2f291475
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-25 08:36:12 +0300

    SpecificMoney created

---
+++ CodeMoreFunction.Core/Empty.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeMoreFunction.Core
+{
+    internal class Empty : SpecificMoney
+    {
+        public Empty(Currency currency) : base(currency)
+        {
+        }
+    }
+}
---
+++ CodeMoreFunction.Core/SpecificMoney.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodeMoreFunction.Core
+{
+    public abstract class SpecificMoney : Money
+    {
+        public Currency Currency { get; }
+
+        protected SpecificMoney(Currency currency)
+        {
+            Currency = currency ?? throw new ArgumentNullException(nameof(currency));
+        }
+
+        public override SpecificMoney Of(Currency currency)
+        {
+            if (currency.Equals(this.Currency))
+                return this;
+            return new Empty(currency);
+        }
+
+        public abstract Tuple<Amount, Money> Take(decimal amount);
+
+    }
+}
